21ffc56446419ff0bee3848cae66c0233663b094
IrinaLukyanova <forks1998@mail.ru>
2020-03-18 17:49:27 +0300

    Попытка настроить и использовать Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-sass");
+
+  grunt.initConfig({
+    sass: {
+      style: {
+        files: {
+          "css/style.css": "sass/style.scss"
+        }
+      }
+    }
+  });
+};
